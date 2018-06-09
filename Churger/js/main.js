c4c937f9e9f05b4223ab66798f15f3373d49cfc1
TverdVV <tverdokhlebov0011@gmail.com>
2018-06-09 23:59:27 +0300

    churger

---
+++ Churger/js/main.js
@@ -0,0 +1,14 @@
+// $(document).ready(function () {
+// 	$('.direction-blocks').slick({
+// 		infinite: true,
+// 		centrMode: true,
+// 		arrows: true,
+// 		prevArrow: '<span class="prev" style="color: #fff;">prev</span>',
+// 		nextArrow: '<span class="next" style="color: #fff;">next</span>',
+// 		autoplay: true,
+// 		autoplaySpeed: 5000
+// 	});
+// 	$('#menu').click(function() {
+// 		$('.menu').toggleClass("menu_active");
+// 	});
+// });
